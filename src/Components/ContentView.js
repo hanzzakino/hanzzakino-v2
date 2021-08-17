10d39a015de993323bf4a10697a51b1a4cefb4d8
hanzzakino <emperornezl50@gmail.com>
2021-08-17 19:05:22 +0800

    Minor Fix: Fixed not visible footer in build

--- src/Components/ContentView.js
+++ src/Components/ContentView.js
@@ -139,48 +139,6 @@ class ContentView extends React.Component {
 
       <br /><br /><br /><br />
 
-      <div className="hz-container">
-        <div className="hz-row-unresponsive">
-          <div className="hz-column-fit">
-
-            <div className="hz-line-ci">
-              <div className="hz-circle H-Ctn-circle1"></div>
-              <div className="hz-line H-Ctn-line1"></div>
-            </div>
-
-          </div>
-          <div className="hz-column">
-            <h2 className="hz-content-programminng-title">Programming</h2>
-            <h3 className="hz-content-programminng-web-title">Web Development</h3>
-
-            <p className="hz-content-text" id="web">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet erat convallis, ultricies diam ultrices, lacinia purus. In dictum lectus eu massa interdum tempor. Curabitur dictum rhoncus ligula eu vehicula. Sed fringilla blandit lorem nec scelerisque. Pellentesque malesuada ipsum in orci lobortis, et ullamcorper quam interdum. Fusce volutpat, mauris et convallis maximus, odio massa interdum dui, ut tempus tellus arcu ut velit. Aenean placerat et purus id semper. Ut interdum eleifend pellentesque. Aenean ac malesuada augue. Mauris tempus ex vitae ante interdum, id sodales ipsum congue.
-            <br /> <br />
-            Pellentesque ac elit et neque vestibulum luctus vel vitae mi. Donec sapien tellus, maximus eget egestas non, scelerisque eu metus. Proin aliquam nibh dictum nibh ultrices, non scelerisque sem dictum. Cras eget pretium lorem. Etiam blandit, lacus non consectetur venenatis, lorem magna placerat felis, eu aliquam nulla nisl ac tortor. Etiam id scelerisque nisi. Nam quis dolor quis justo suscipit suscipit. Curabitur laoreet tincidunt nisi sit amet ornare. Proin ut velit sit amet eros condimentum dignissim. In imperdiet felis et hendrerit convallis. Nullam dapibus massa pharetra elit porta dignissim. Maecenas a eleifend risus, vel commodo eros. Pellentesque dictum commodo erat, consequat sagittis velit euismod quis. Etiam finibus eu lectus eu aliquet. Morbi erat sem, rhoncus interdum rhoncus nec, sagittis ut lacus.
-            <br /> <br />
-            Vestibulum fermentum vulputate posuere. In fermentum magna eu suscipit faucibus. Suspendisse augue nibh, ultricies sit amet orci in, suscipit posuere dui. Sed at turpis in libero mattis lacinia. Sed quis posuere ex. In vitae finibus tortor, quis finibus massa. In velit metus, dictum id semper vitae, feugiat a risus. Aenean venenatis pretium risus tempus tincidunt. Aliquam venenatis sodales mi, non auctor arcu. Fusce malesuada elit ante, non sagittis tortor tincidunt at. Fusce nec mauris tincidunt, ultrices purus id, maximus purus. Etiam et dui dictum, sollicitudin libero non, porta felis. Ut faucibus consequat justo eu mattis.
-            <br /> <br />
-            Nam et sodales eros, a imperdiet elit. Morbi purus nibh, rutrum non tempus id, condimentum ac quam. Suspendisse potenti. Pellentesque ut massa eu neque placerat vehicula et mattis erat. Duis facilisis nec enim quis imperdiet. Phasellus blandit ante vel leo pretium consequat. Phasellus sem massa, luctus vitae faucibus vel, venenatis non elit. Proin scelerisque eu magna a tincidunt.
-            <br /> <br />
-            Cras semper dolor ut magna maximus, vestibulum finibus elit cursus. Nullam id nulla eget orci vestibulum hendrerit. Fusce facilisis tincidunt quam, nec fermentum magna maximus et. Aliquam sed sagittis ante. Mauris vestibulum enim venenatis rutrum lobortis. Suspendisse sollicitudin congue tincidunt. Quisque id volutpat nibh.
-            <br /> <br />
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet erat convallis, ultricies diam ultrices, lacinia purus. In dictum lectus eu massa interdum tempor. Curabitur dictum rhoncus ligula eu vehicula. Sed fringilla blandit lorem nec scelerisque. Pellentesque malesuada ipsum in orci lobortis, et ullamcorper quam interdum. Fusce volutpat, mauris et convallis maximus, odio massa interdum dui, ut tempus tellus arcu ut velit. Aenean placerat et purus id semper. Ut interdum eleifend pellentesque. Aenean ac malesuada augue. Mauris tempus ex vitae ante interdum, id sodales ipsum congue.
-            <br /> <br />
-            Pellentesque ac elit et neque vestibulum luctus vel vitae mi. Donec sapien tellus, maximus eget egestas non, scelerisque eu metus. Proin aliquam nibh dictum nibh ultrices, non scelerisque sem dictum. Cras eget pretium lorem. Etiam blandit, lacus non consectetur venenatis, lorem magna placerat felis, eu aliquam nulla nisl ac tortor. Etiam id scelerisque nisi. Nam quis dolor quis justo suscipit suscipit. Curabitur laoreet tincidunt nisi sit amet ornare. Proin ut velit sit amet eros condimentum dignissim. In imperdiet felis et hendrerit convallis. Nullam dapibus massa pharetra elit porta dignissim. Maecenas a eleifend risus, vel commodo eros. Pellentesque dictum commodo erat, consequat sagittis velit euismod quis. Etiam finibus eu lectus eu aliquet. Morbi erat sem, rhoncus interdum rhoncus nec, sagittis ut lacus.
-            <br /> <br />
-            Vestibulum fermentum vulputate posuere. In fermentum magna eu suscipit faucibus. Suspendisse augue nibh, ultricies sit amet orci in, suscipit posuere dui. Sed at turpis in libero mattis lacinia. Sed quis posuere ex. In vitae finibus tortor, quis finibus massa. In velit metus, dictum id semper vitae, feugiat a risus. Aenean venenatis pretium risus tempus tincidunt. Aliquam venenatis sodales mi, non auctor arcu. Fusce malesuada elit ante, non sagittis tortor tincidunt at. Fusce nec mauris tincidunt, ultrices purus id, maximus purus. Etiam et dui dictum, sollicitudin libero non, porta felis. Ut faucibus consequat justo eu mattis.
-            <br /> <br />
-            Nam et sodales eros, a imperdiet elit. Morbi purus nibh, rutrum non tempus id, condimentum ac quam. Suspendisse potenti. Pellentesque ut massa eu neque placerat vehicula et mattis erat. Duis facilisis nec enim quis imperdiet. Phasellus blandit ante vel leo pretium consequat. Phasellus sem massa, luctus vitae faucibus vel, venenatis non elit. Proin scelerisque eu magna a tincidunt.
-            <br /> <br />
-            Cras semper dolor ut magna maximus, vestibulum finibus elit cursus. Nullam id nulla eget orci vestibulum hendrerit. Fusce facilisis tincidunt quam, nec fermentum magna maximus et. Aliquam sed sagittis ante. Mauris vestibulum enim venenatis rutrum lobortis. Suspendisse sollicitudin congue tincidunt. Quisque id volutpat nibh.
-            </p>
-          </div>
-        </div>
-      </div>
-
-
-
-
     </div>);
   }
   AboutMe(){
@@ -257,10 +215,10 @@ class ContentView extends React.Component {
       </div>
     );
   }
+
   Footer(){
     var hFLogo = {
-      color: "white",
-      opacity: "70%",
+      color: "rgb(180,180,180)",
     };
 
 
